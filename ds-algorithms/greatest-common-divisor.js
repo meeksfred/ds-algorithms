c8ddf2cdbbef6a49633b8c630693b76494ffd20e
meeksfred <maxfred0025@gmail.com>
2017-08-27 16:22:11 -0700

    greatest common divisor

---
+++ ds-algorithms/greatest-common-divisor.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Find the greatest common divisor between two numbers.
+
+function greatestCommonDivisor(a, b) {
+  let denominator = 2;
+  let answer = 1;
+
+  while (a >= denominator && b >= denominator) {
+    if (a % denominator === 0 && b % denominator === 0) {
+      answer = denominator;
+    }
+    denominator++;
+  }
+
+  console.log(answer);
+}
+
+greatestCommonDivisor(24, 36);
+greatestCommonDivisor(124, 245);
+greatestCommonDivisor(244, 424);
