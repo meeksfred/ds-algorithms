0b3869bf4c2bd4493b66289b23be86e3abba4c6d
meeksfred <maxfred0025@gmail.com>
2017-08-28 15:57:35 -0700

    convert string to acronym

---
+++ ds-algorithms/convert-to-acronym.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// not working locally because I need Node version 6 or higher.
+let ignoreList = ['the', 'a', 'of', 'and', 'or', 'to', 'with', 'an'];
+
+function acronyms(string) {
+  let acronym = '';
+  let newStr = string.split(' ');
+
+  for(let i=0; i<newStr.length; i++) {
+    if (!ignoreList.includes(newStr[i].toLowerCase())) {
+      acronym += newStr[i][0];
+    }
+  }
+
+  return acronym;
+}
+
+console.log(acronyms('The Federal Bureau of Investigation'));
