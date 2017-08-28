18cc116f1e6abf10b1284e9c858cfe4715d97f1c
meeksfred <maxfred0025@gmail.com>
2017-08-28 15:25:05 -0700

    new solution left rotation

--- ds-algorithms/left-rotation.js
+++ ds-algorithms/left-rotation.js
@@ -1,18 +1,31 @@
 'use strict';
 
-let nums = [2, 5, 12, 6, 33, 23];
-let rotations = 3;
+// let nums = [2, 5, 12, 6, 33, 23];
+// let rotations = 3;
+//
+// function leftRotation(array, d) {
+//     let rotated = [];
+//
+//     for(let i=0; i<array.length; i++) {
+//         let newIndex = (i + (array.length - d)) % array.length;
+//         rotated[newIndex] = array[i];
+//     }
+//
+//     return rotated;
+// }
+//
+//
+// console.log(leftRotation(nums, rotations));
 
-function leftRotation(array, d) {
-    let rotated = [];
-
-    for(let i=0; i<array.length; i++) {
-        let newIndex = (i + (array.length - d)) % array.length;
-        rotated[newIndex] = array[i];
+function leftRotation(array, rotations) {
+  while(rotations--) {
+    array.push(array[0]);
+    for (let i = 0; i < array.length - 1; i++) {
+      array[i] = array[i+1];
     }
-
-    console.log(rotated);
+    array.pop();
+  }
+  return array;
 }
 
-
-leftRotation(nums, rotations);
+console.log(leftRotation([1, 2, 3, 4, 5, 6], 4));
